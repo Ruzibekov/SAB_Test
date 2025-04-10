bbb197dc99d4ec383050ed0defcf78c6ca4e572e
Shavkat <ruzibekov44@gmail.com>
2025-04-10 19:22:03 +0500

    feat: integrate Roboto font family for consistent typography

--- app/src/main/java/com/ruzibekov/sab_test/MainActivity.kt
+++ app/src/main/java/com/ruzibekov/sab_test/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ruzibekov.sab_test.screens.address.MyAddressesScreen
+import com.ruzibekov.sab_test.screens.map.MapScreen
 import com.ruzibekov.sab_test.ui.theme.SAB_TestTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,4 +27,36 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = {
+            MainBottomNavigationView()
+        }
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Addresses.route,
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            composable(Screen.Addresses.route) { MyAddressesScreen() }
+            composable(Screen.Favorites.route) { MapScreen() }
+            composable(Screen.Profile.route) { }
+        }
+    }
+}
+
+@Composable
+private fun MainBottomNavigationView() {
+
+}
+
+@Preview
+@Composable
+private fun MainContentPreview() {
+    SAB_TestTheme { MainScreen() }
 }
--- app/src/main/java/com/ruzibekov/sab_test/components/TopBarView.kt
+++ app/src/main/java/com/ruzibekov/sab_test/components/TopBarView.kt
@@ -1,13 +1,17 @@
 package com.ruzibekov.sab_test.components
 
 import androidx.annotation.StringRes
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.ruzibekov.sab_test.R
 
 @Composable
 fun TopBarView(@StringRes titleRes: Int) {
-
+    Text(
+        text = stringResource(titleRes),
+    )
 }
 
 @Preview
