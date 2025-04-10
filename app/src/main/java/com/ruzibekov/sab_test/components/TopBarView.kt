e842d4e8b1391ed0f4e9fc8cba25a2ac6f321e6a
Shavkat <ruzibekov44@gmail.com>
2025-04-10 19:35:59 +0500

    add: TopBarView.kt

--- app/src/main/java/com/ruzibekov/sab_test/components/TopBarView.kt
+++ app/src/main/java/com/ruzibekov/sab_test/components/TopBarView.kt
@@ -1,23 +1,44 @@
 package com.ruzibekov.sab_test.components
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ruzibekov.sab_test.R
+import com.ruzibekov.sab_test.ui.theme.SAB_TestTheme
 
 @Composable
 fun TopBarView(@StringRes titleRes: Int) {
-    Text(
-        text = stringResource(titleRes),
-    )
+    Surface(
+        shape = RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp),
+        shadowElevation = 1.dp
+    ) {
+        Text(
+            text = stringResource(titleRes),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 18.dp, bottom = 15.dp),
+            style = MaterialTheme.typography.labelMedium,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Preview
 @Composable
 private fun TopBarPreview() {
-    TopBarView(
-        titleRes = R.string.my_addresses
-    )
+    SAB_TestTheme {
+        TopBarView(
+            titleRes = R.string.my_addresses
+        )
+    }
 }
